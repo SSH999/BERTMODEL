3a16efe636f9a6997882f3085b7cb241ac882613
SSH999 <124814424+SSH999@users.noreply.github.com>
2023-04-02 23:20:49 +0530

    Update sample.py

--- BERT MODEL/sample.py
+++ BERT MODEL/sample.py
@@ -1,4 +1,4 @@
-Here are some sample predictions made by our model and their explanations:
+Here are some sample predictions made by the model and their explanations:
 
 Text: "The trade market is performing well  today."
 Prediction: Finance
@@ -10,7 +10,7 @@ Explanation: The text contains words related to travel and vacation, which fall 
 
 Text: "I just finished reading a book on Environmental science."
 Prediction: Science
-Explanation: The text contains words related to science, which falls under the ScienceS category.
+Explanation: The text contains words related to science, which falls under the Science category.
 
 
 
